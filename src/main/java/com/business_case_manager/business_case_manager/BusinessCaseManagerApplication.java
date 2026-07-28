afdf6395d51b1435a3a355a599a1520f69ff8951
Hamza Ahmyttou <hamza.ahmyttou@outlook.com>
2026-07-28 12:58:14 +0100

    Spring initializr

---
+++ src/main/java/com/business_case_manager/business_case_manager/BusinessCaseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.business_case_manager.business_case_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCaseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCaseManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/business_case_manager/business_case_manager/BusinessCaseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business_case_manager.business_case_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessCaseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
